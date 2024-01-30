ce901879212462dd9f549746e52e0e7f51f98761
SometimesThinks <76872109+SometimesThinks@users.noreply.github.com>
2024-01-30 23:45:28 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+coin_lst = []
+coin_cnt = 0
+
+for _ in range(n):
+    coin_lst.append(int(input())) # 동전 리스트에 동전 입력 받기
+
+for coin in coin_lst[::-1]: # 큰 동전부터 반복문 돌면서 먼저 사용하기
+    if k <= 0:
+        break
+    if k >= coin:
+        coin_cnt += k // coin
+        k %= coin
+print(coin_cnt)
