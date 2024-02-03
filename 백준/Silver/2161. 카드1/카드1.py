d9880abd1a53a128c80d193c8ca43621c0888cd2
SometimesThinks <76872109+SometimesThinks@users.noreply.github.com>
2024-02-03 22:39:44 +0900

    [Silver V] Title: 카드1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,8 @@
+n = int(input())
+lst = list(range(1, n + 1)) # 1부터 n까지의 리스트 선언
+
+while len(lst) > 1:
+    print(lst.pop(0), end=' ') # pop 메소드는 요소 위치 삭제 후 삭제한 값 반환
+    lst.append(lst.pop(0))
+
+print(*lst)
