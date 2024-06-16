bfc369b511a6fd4727827e25ad423175b52e254a
SometimesThinks <76872109+SometimesThinks@users.noreply.github.com>
2024-06-16 17:18:48 +0900

    [Silver II] Title: N과 M (12), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(start, depth, seq):
+    if depth == m:
+        if tuple(seq) not in made_set:
+            made_set.add(tuple(seq))
+            print(*seq)
+            return
+        else:
+            return
+    for i in range(n):
+        if lst[i] >= lst[start]:
+            dfs(i, depth + 1, seq + [lst[i]])
+
+
+n, m = map(int, input().rstrip().split())
+lst = list(map(int, input().rstrip().split()))
+made_set = set()
+
+lst.sort()
+dfs(0, 0, [])
