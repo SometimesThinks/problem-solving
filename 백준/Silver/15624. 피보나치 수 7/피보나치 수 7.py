99b23a2b2756bf75a167309550639ba08ed10564
DAEYEONG_YUN <76872109+SometimesThinks@users.noreply.github.com>
2024-07-14 00:54:37 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 372 ms, Memory: 71112 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 
 n = int(input().rstrip())
 dp = [0 for _ in range(n + 1)]
-dp[0] = 1
+dp[0] = 0
 
 for i in range(1, n + 1):
     if i == 1:
@@ -12,4 +12,4 @@ for i in range(1, n + 1):
     else:
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007
 
-print(dp[n - 1])
+print(dp[n])
