3541accf7e113cd947cbe691cae49d944515cfc8
DAEYEONG_YUN <ycw2859@gmail.com>
2025-01-24 19:19:18 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 104 ms, Memory: 9684 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+
+for (let i = n; i > 0; i--) {
+  console.log(`${' '.repeat(n - i)}${'*'.repeat(2 * i - 1)}`);
+}
+for (let i = 2; i < n + 1; i++) {
+  console.log(`${' '.repeat(n - i)}${'*'.repeat(2 * i - 1)}`);
+}
