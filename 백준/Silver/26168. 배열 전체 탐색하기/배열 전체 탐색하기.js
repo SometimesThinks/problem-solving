4cd20b0a0aec3dfdd477552182d12fa39b04985d
DAEYEONG_YUN <ycw2859@gmail.com>
2025-01-30 00:40:45 +0900

    [Silver IV] Title: 배열 전체 탐색하기, Time: 3960 ms, Memory: 80264 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.js
+++ 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.js
@@ -10,24 +10,58 @@ const seq = input[1]
 const questions = input
   .slice(2)
   .map((question) => question.split(' ').map(Number));
+const findMore = (arr, target) => {
+  let left = 0;
+  let right = arr.length;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] >= target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
+const findOver = (arr, target) => {
+  let left = 0;
+  let right = arr.length;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] > target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
+const findBelow = (arr, target) => {
+  let left = 0;
+  let right = arr.length;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] <= target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left - 1;
+};
 
 for (let i = 0; i < m; i++) {
   const quNum = questions[i][0];
   let cnt = 0;
-  for (let j = 0; j < n; j++) {
-    if (quNum === 1) {
-      if (seq[j] >= questions[i][1]) {
-        cnt += 1;
-      }
-    } else if (quNum === 2) {
-      if (seq[j] > questions[i][1]) {
-        cnt += 1;
-      }
-    } else {
-      if (seq[j] >= questions[i][1] && seq[j] <= questions[i][2]) {
-        cnt += 1;
-      }
-    }
+  if (quNum === 1) {
+    cnt = n - findMore(seq, questions[i][1]);
+  } else if (quNum === 2) {
+    cnt = n - findOver(seq, questions[i][1]);
+  } else {
+    cnt = findBelow(seq, questions[i][2]) - findMore(seq, questions[i][1]) + 1;
   }
   console.log(cnt);
 }
