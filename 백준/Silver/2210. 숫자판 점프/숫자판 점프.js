cf40bdff3a86147a016707e1cffa84f01484627c
DAEYEONG_YUN <ycw2859@gmail.com>
2025-02-08 00:29:47 +0900

    [Silver II] Title: 숫자판 점프, Time: 168 ms, Memory: 12112 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const { markAsUntransferable } = require('worker_threads');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const matrix = input.map((row) => row.split(' ').map(Number));
+const ans = new Set();
+const dx = [0, 0, -1, 1];
+const dy = [-1, 1, 0, 0];
+const dfs = (strX, strY, res) => {
+  if (res.length === 6) {
+    ans.add(res);
+    return;
+  }
+  for (let i = 0; i < 4; i++) {
+    const tempX = strX + dx[i];
+    const tempY = strY + dy[i];
+
+    if (tempX >= 0 && tempX < 5 && tempY >= 0 && tempY < 5) {
+      dfs(tempX, tempY, res + matrix[tempX][tempY]);
+    }
+  }
+};
+
+for (let r = 0; r < 5; r++) {
+  for (let c = 0; c < 5; c++) {
+    dfs(r, c, '');
+  }
+}
+console.log(ans.size);
