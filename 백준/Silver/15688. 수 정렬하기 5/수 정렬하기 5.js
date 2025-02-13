abc7d3a91eb89ccdfc892312037095cdc4859852
DAEYEONG_YUN <ycw2859@gmail.com>
2025-02-13 18:50:41 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 17652 ms, Memory: 220472 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+const seq = [];
+
+for (let i = 1; i < n + 1; i++) {
+  seq.push(Number(input[i]));
+}
+console.log(seq.sort((a, b) => a - b).join('\n'));
