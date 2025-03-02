cd8a683a02088d33cb44a60a180b6ee6ee33c9bb
DAEYEONG_YUN <ycw2859@gmail.com>
2025-03-03 03:10:47 +0900

    [Bronze IV] Title: Next in line, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+console.log(+input[1] + +input[1] - +input[0]);
