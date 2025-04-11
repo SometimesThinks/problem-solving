669939e93b8a7e23c0fc236dcb8d6116f7a53010
DAEYEONG_YUN <ycw2859@gmail.com>
2025-04-11 17:17:49 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 92 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+console.log(Number(input[0]) ** 3);
