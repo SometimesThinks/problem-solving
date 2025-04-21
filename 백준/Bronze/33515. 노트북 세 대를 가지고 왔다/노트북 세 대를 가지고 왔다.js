7dbe4c3b8162e6bc61b58769890d924d9ffab696
DAEYEONG_YUN <ycw2859@gmail.com>
2025-04-21 14:27:57 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const times = input[0]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+console.log(times[0]);
