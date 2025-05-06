66d8045e70314a2eb87eebaae193426332832434
DAEYEONG_YUN <ycw2859@gmail.com>
2025-05-06 17:45:34 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int sum = a + b;
+
+    if (sum == c) {
+        cout << "correct!" << endl;
+    } else {
+        cout << "wrong!" << endl;
+    }
+
+    return 0;
+}
