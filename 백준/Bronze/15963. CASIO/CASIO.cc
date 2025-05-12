2c4112a94ac908c327df5c31fe9b968fef715b16
DAEYEONG_YUN <ycw2859@gmail.com>
2025-05-13 02:07:53 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    long long n, m;
+    cin >> n >> m;
+
+    cout << (m >= n ? 1 : 0) << endl;
+    return 0;
+}
