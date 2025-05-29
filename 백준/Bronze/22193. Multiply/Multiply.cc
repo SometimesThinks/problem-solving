da2ceef58f2f500df927c5e0aa10319df1d4d6a0
DAEYEONG_YUN <ycw2859@gmail.com>
2025-05-29 19:29:29 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    long long a, b;
+    cin >> a;
+    cin >> b;
+
+    cout << a * b << endl;
+    return 0;
+}
