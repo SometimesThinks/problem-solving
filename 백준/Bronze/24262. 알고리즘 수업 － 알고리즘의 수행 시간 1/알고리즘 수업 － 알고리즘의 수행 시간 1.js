a30d0e553a529f3dbe43ae5e036727c06cad4f29
DAEYEONG_YUN <ycw2859@gmail.com>
2025-06-11 08:16:46 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+
+console.log(1);
+console.log(0);
