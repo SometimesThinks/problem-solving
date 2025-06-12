523ce7df3a42205fe65872d03e5769fed8a22911
DAEYEONG_YUN <ycw2859@gmail.com>
2025-06-13 06:55:29 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 128 ms, Memory: 9304 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+console.log('비와이');
