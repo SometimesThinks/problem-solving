f90fa9e47762d69b53500cd2caf5d24dfc3e7936
DAEYEONG_YUN <ycw2859@gmail.com>
2025-07-06 12:47:29 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 92 ms, Memory: 9408 KB -BaekjoonHub

---
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+let res = '';
+
+for (let i = 0; i < Math.floor(n / 5); i++) res += 'V';
+for (let i = 0; i < Math.floor(n % 5); i++) res += 'I';
+console.log(res);
