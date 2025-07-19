503f4a3232567b2678ced809f12031ccf5c491bb
DAEYEONG_YUN <ycw2859@gmail.com>
2025-07-19 17:19:13 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 92 ms, Memory: 9424 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+const s = input[1];
+
+console.log(s.slice(n - 5));
