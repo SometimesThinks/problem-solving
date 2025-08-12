8835c9418a5ea0a10b38e4c125d1899b60ea7d98
DAEYEONG_YUN <ycw2859@gmail.com>
2025-08-12 15:32:02 +0900

    [Bronze IV] Title: 특별한 가지, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const [n, m, k] = input.map(Number);
+
+console.log((m / n) * k);
