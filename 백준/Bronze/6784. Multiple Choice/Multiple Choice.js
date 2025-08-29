b0c7c83705dffb9ff8540945ec6517a55f30512b
DAEYEONG_YUN <ycw2859@gmail.com>
2025-08-29 13:08:15 +0900

    [Bronze IV] Title: Multiple Choice, Time: 92 ms, Memory: 9508 KB -BaekjoonHub

---
+++ 백준/Bronze/6784. Multiple Choice/Multiple Choice.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+let cnt = 0;
+
+for (let i = 1; i < n + 1; i++) {
+  if (input[i] === input[i + n]) cnt++;
+}
+console.log(cnt);
