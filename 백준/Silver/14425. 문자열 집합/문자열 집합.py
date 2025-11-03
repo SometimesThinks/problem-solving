54e96c8f05a4b54724a95ebc0b229db3330e4a90
DAEYEONG_YUN <ycw2859@gmail.com>
2025-11-03 14:36:52 +0900

    [Silver IV] Title: 문자열 집합, Time: 88 ms, Memory: 37440 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+s = set()
+cnt = 0
+
+for _ in range(n):
+    s.add(input().rstrip())
+for _ in range(m):
+    if input().rstrip() in s:
+        cnt += 1
+print(cnt)
