f72d1e56a28623305e58491554358eb3c3e4ca0c
DAEYEONG_YUN <ycw2859@gmail.com>
2025-11-30 17:35:05 +0900

    [Bronze III] Title: 홀수일까 짝수일까, Time: 92 ms, Memory: 9500 KB -BaekjoonHub

---
+++ 백준/Bronze/5988. 홀수일까 짝수일까/홀수일까 짝수일까.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+
+for (let i = 1; i < n + 1; i++) {
+  const k = BigInt(input[i]);
+  console.log(k % BigInt(2) ? 'odd' : 'even');
+}
