49a78383e735a4a881a7bb533ea0157d4395b5da
DAEYEONG_YUN <ycw2859@gmail.com>
2025-12-16 06:53:37 +0900

    [Bronze V] Title: 이중 반복문, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+
+if (n ** 2 <= 10 ** 8) console.log('Accepted');
+else console.log('Time limit exceeded');
