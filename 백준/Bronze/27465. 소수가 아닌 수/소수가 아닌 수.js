6749be224ea57c130d0ee00018c5515f6177ff74
DAEYEONG_YUN <ycw2859@gmail.com>
2026-01-13 13:31:56 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 92 ms, Memory: 9588 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.js
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.js
@@ -2,4 +2,11 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? 0 : './input.txt';
 const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
 
-console.log(input[0] + '2');
+const n = Number(input[0]);
+
+if (n === 1) {
+  console.log(4);
+} else if (n === 2 || n === 3) {
+  console.log(6);
+} else if (n % 2 === 0) console.log(n);
+else console.log(n + 1);
