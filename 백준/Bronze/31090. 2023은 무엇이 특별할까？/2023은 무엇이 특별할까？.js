97c1a6af6c29b7a4d43c6fef29378487c40b7355
DAEYEONG_YUN <ycw2859@gmail.com>
2026-02-01 09:45:34 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 512 ms, Memory: 13744 KB -BaekjoonHub

---
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+
+for (let i = 1; i < n + 1; i++) {
+  const next = Number(input[i]) + 1;
+  const end = Number(input[i].slice(2));
+
+  if (next % end) console.log('Bye');
+  else console.log('Good');
+}
