9d1d51976c9393eaf932cbf3783eb50b301e16c7
DAEYEONG_YUN <ycw2859@gmail.com>
2026-02-02 11:07:54 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스 2, Time: 96 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Bronze/26068. 치킨댄스를 추는 곰곰이를 본 임스 2/치킨댄스를 추는 곰곰이를 본 임스 2.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : './input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').trim().split('\n');
+
+const n = Number(input[0]);
+let cnt = 0;
+
+for (let i = 1; i < n + 1; i++) {
+  const [_, day] = input[i].split('-');
+  if (Number(day) <= 90) cnt++;
+}
+console.log(cnt);
